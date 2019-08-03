4e08da4a4d2131eb0d7128d4373bd2ec0ce26e2d
DaltonSherwin <daltonsherwin@gmail.com>
2019-08-03 01:46:27 +0000

    Splurty lesson 16

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,9 +3,7 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
+
 
   def create
     @quote = Quote.create(quote_params)
